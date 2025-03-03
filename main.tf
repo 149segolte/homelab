fd90031ada4303c069a02edaccbc7a349a49893a
149segolte <37300847+149segolte@users.noreply.github.com>
2025-03-02 19:34:15 -0500

    refactor: use assertions with HCP Vault

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  backend "local" {
+    # Backup using any utilities like rsync, scp, rclone, syncthing, etc.
+    path = "/Users/one49segolte/Documents/terraform_states/homelab.tfstate"
+  }
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "4.6.0"
+    }
+    assert = {
+      source  = "hashicorp/assert"
+      version = "0.15.0"
+    }
+  }
+}
+
 variable "data_terraform" {
   description = "Path to a dir which contains terraform related files"
   type        = string
@@ -15,17 +32,10 @@ variable "vault_password" {
   sensitive   = true
 }
 
-terraform {
-  backend "local" {
-    # Backup using any utilities like rsync, scp, rclone, syncthing, etc.
-    path = "/Users/one49segolte/Documents/terraform_states/homelab.tfstate"
-  }
-  required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "4.6.0"
-    }
-  }
+variable "kv_store_path" {
+  description = "The path to the kv store"
+  type        = string
+  default     = "homelab/terraform"
 }
 
 provider "vault" {
@@ -40,8 +50,40 @@ provider "vault" {
 
 data "vault_generic_secret" "healthcheck" {
   path = "sys/health"
+  lifecycle {
+    postcondition {
+      condition     = provider::assert::true(self.data.initialized)
+      error_message = "The vault instance is not initialized"
+    }
+    postcondition {
+      condition     = provider::assert::false(self.data.sealed)
+      error_message = "The vault instance is sealed"
+    }
+  }
 }
 
-output "health" {
-  value = nonsensitive(data.vault_generic_secret.healthcheck.data)
+# output "vault_healthcheck" {
+#   value = nonsensitive(data.vault_generic_secret.healthcheck.data)
+# }
+
+data "vault_generic_secret" "kv_store" {
+  path = "sys/mounts/${var.kv_store_path}"
+  lifecycle {
+    postcondition {
+      condition     = provider::assert::true(self.data.type == "kv")
+      error_message = "secret engine ${var.kv_store_path} is not a kv store"
+    }
+    postcondition {
+      condition     = provider::assert::valid_json(self.data.options)
+      error_message = "kv store ${var.kv_store_path} options are not valid JSON"
+    }
+    postcondition {
+      condition     = provider::assert::true(jsondecode(self.data.options).version == "2")
+      error_message = "kv store ${var.kv_store_path} is not version 2"
+    }
+  }
 }
+
+# output "kv_store" {
+#   value = nonsensitive(data.vault_generic_secret.kv_store.data)
+# }
