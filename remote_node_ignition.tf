9388d0ff5920c29a4b19014e0197b4479807e6b5
149segolte <37300847+149segolte@users.noreply.github.com>
2025-03-06 04:27:11 -0500

    feat: expand remote config for setup service

--- remote_node_ignition.tf
+++ remote_node_ignition.tf
@@ -3,6 +3,12 @@ data "ignition_config" "remote_node" {
   files = [
     data.ignition_file.hostname.rendered,
     data.ignition_file.zram0.rendered,
+    data.ignition_file.sysctl_tailscale.rendered,
+  ]
+  systemd = [
+    data.ignition_systemd_unit.ethtool.rendered,
+    data.ignition_systemd_unit.docker.rendered,
+    data.ignition_systemd_unit.setup.rendered,
   ]
 }
 
@@ -27,4 +33,57 @@ data "ignition_file" "zram0" {
   content {
     content = "# This config file enables a /dev/zram0 device with the defaults\n[zram0]\n"
   }
+}
+
+data "ignition_file" "sysctl_tailscale" {
+  path = "/etc/sysctl.d/99-tailscale.conf"
+  mode = 420
+  content {
+    content = "net.ipv4.ip_forward=1\nnet.ipv6.conf.all.forwarding=1\n"
+  }
+}
+
+data "ignition_systemd_unit" "ethtool" {
+  name    = "ethtool@.service"
+  enabled = false
+  content = <<-EOT
+    [Unit]
+    Description=Ethtool configuration to use transport layer offloads
+    Wants=network-online.target
+    After=network-online.target
+
+    [Service]
+    Type=oneshot
+    ExecStart=/usr/sbin/ethtool -K %i rx-udp-gro-forwarding on rx-gro-list off
+
+    [Install]
+    WantedBy=multi-user.target
+  EOT
+}
+
+data "ignition_systemd_unit" "docker" {
+  name = "docker.service"
+  mask = true
+}
+
+data "ignition_systemd_unit" "setup" {
+  name    = "setup.service"
+  enabled = true
+  content = <<-EOT
+    [Unit]
+    Description=Configure system on first boot
+    Wants=network-online.target
+    After=network-online.target
+    ConditionPathExists=!/var/lib/%N.stamp
+
+    [Service]
+    Type=oneshot
+    RemainAfterExit=yes
+    ExecStart=/bin/bash -c "/bin/systemctl enable --now ethtool@$(ip -o route get 8.8.8.8 | cut -f 5 -d ' ').service"
+    ExecStart=/bin/systemctl enable --now podman.socket
+    ExecStart=/bin/touch /var/lib/%N.stamp
+
+    [Install]
+    WantedBy=multi-user.target
+  EOT
 }
